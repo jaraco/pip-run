143500bc7064b05d0ce4a132cce1f48ddb1ecee7
Jason R. Coombs <jaraco@jaraco.com>
2016-01-25 23:14:00 -0500

    Add test for basic functionality

---
+++ rwt/tests/test_scripts.py
@@ -0,0 +1,20 @@
+import textwrap
+import sys
+import subprocess
+
+
+def test_pkg_imported(tmpdir):
+	"""
+	Create a script that loads cython and ensure it runs.
+	"""
+	body = textwrap.dedent("""
+		import cython
+		print("Successfully imported cython")
+		""").lstrip()
+	script_file = tmpdir / 'script'
+	script_file.write_text(body, 'utf-8')
+	pip_args = ['cython']
+	cmd = [sys.executable, '-m', 'rwt'] + pip_args + ['--', str(script_file)]
+
+	out = subprocess.check_output(cmd, universal_newlines=True)
+	assert 'Successfully imported cython' in out
