e947e9b0a5c3804d50bc6a602433861cca10debb
Jason R. Coombs <jaraco@jaraco.com>
2016-01-26 22:39:23 -0500

    Add test capturing expectation that entry points will be visible after installing packages.

---
+++ rwt/tests/test_deps.py
@@ -0,0 +1,13 @@
+import pytest
+import pkg_resources
+
+from rwt import deps
+
+@pytest.mark.xfail(reason="Technique fails to load entry points")
+def test_entry_points():
+	"""
+	Ensure entry points are visible after making packages visible
+	"""
+	with deps.on_sys_path('jaraco.mongodb'):
+		eps = pkg_resources.iter_entry_points('pytest11')
+		assert list(eps), "Entry points not found"
