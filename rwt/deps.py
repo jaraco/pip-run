0d44bcb50462dcee27b6f6cf0fd06bc12428fa39
Jason R. Coombs <jaraco@jaraco.com>
2016-02-01 20:23:43 -0500

    Install backport of _initialize_master_working_set to enable functionality on older setuptools.

--- rwt/deps.py
+++ rwt/deps.py
@@ -17,6 +17,8 @@ def _update_working_set():
 	"""
 	try:
 		pkg_resources = sys.modules['pkg_resources']
+		if not hasattr(pkg_resources, '_initialize_master_working_set'):
+			exec(_init_ws_patch, vars(pkg_resources))
 		pkg_resources._initialize_master_working_set()
 	except KeyError:
 		# it's unnecessary to re-initialize when it hasn't
@@ -49,3 +51,37 @@ def on_sys_path(*args):
 	finally:
 		sys.path.remove(target)
 		shutil.rmtree(target)
+
+
+# from setuptools 19.6.2
+_init_ws_patch = '''
+def _initialize_master_working_set():
+    """
+    Prepare the master working set and make the ``require()``
+    API available.
+
+    This function has explicit effects on the global state
+    of pkg_resources. It is intended to be invoked once at
+    the initialization of this module.
+
+    Invocation by other packages is unsupported and done
+    at their own risk.
+    """
+    working_set = WorkingSet._build_master()
+    _declare_state('object', working_set=working_set)
+
+    require = working_set.require
+    iter_entry_points = working_set.iter_entry_points
+    add_activation_listener = working_set.subscribe
+    run_script = working_set.run_script
+    # backward compatibility
+    run_main = run_script
+    # Activate all distributions already on sys.path, and ensure that
+    # all distributions added to the working set in the future (e.g. by
+    # calling ``require()``) will get activated as well.
+    add_activation_listener(lambda dist: dist.activate())
+    working_set.entries=[]
+    # match order
+    list(map(working_set.add_entry, sys.path))
+    globals().update(locals())
+'''
