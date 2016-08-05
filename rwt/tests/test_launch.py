3ca03031599e2a1673c6349710489938f60f6a4d
Jason R. Coombs <jaraco@jaraco.com>
2016-08-05 18:01:09 -0400

    Add test for with_path_overlay also.

--- rwt/tests/test_launch.py
+++ rwt/tests/test_launch.py
@@ -1,8 +1,24 @@
+import sys
+import subprocess
+import textwrap
+
 from rwt import launch
 
 
 def test_with_path(tmpdir, capfd):
-	params = ['-c', 'import sys; print(sys.path)'	]
+	params = ['-c', 'import sys; print(sys.path)']
 	launch.with_path(str(tmpdir), params)
+	out, err = capfd.readouterr()
+	assert str(tmpdir) in out
+
+
+def test_with_path_overlay(tmpdir, capfd):
+	params = ['-c', 'import sys; print(sys.path)']
+	# launch subprocess so as not to overlay the test process
+	script = textwrap.dedent("""
+		import rwt.launch
+		rwt.launch.with_path_overlay({tmpdir!r}, {params!r})
+	""").strip().replace('\n', '; ').format(tmpdir=str(tmpdir), params=params)
+	subprocess.Popen([sys.executable, '-c', script]).wait()
 	out, err = capfd.readouterr()
 	assert str(tmpdir) in out
