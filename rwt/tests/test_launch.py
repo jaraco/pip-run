51bb914287dd5c68a8ba9ab1dc8ea93d72f214b0
Jason R. Coombs <jaraco@jaraco.com>
2016-08-06 10:20:12 -0400

    Demonstrate that cleanup doesn't occur when with_path_overlay is invoked. Ref #4.

--- rwt/tests/test_launch.py
+++ rwt/tests/test_launch.py
@@ -2,6 +2,8 @@ import sys
 import subprocess
 import textwrap
 
+import pytest
+
 from rwt import launch
 
 
@@ -12,13 +14,16 @@ def test_with_path(tmpdir, capfd):
 	assert str(tmpdir) in out
 
 
+@pytest.mark.xfail(reason="cleanup can't occur with execv; #4")
 def test_with_path_overlay(tmpdir, capfd):
 	params = ['-c', 'import sys; print(sys.path)']
 	# launch subprocess so as not to overlay the test process
 	script = textwrap.dedent("""
 		import rwt.launch
 		rwt.launch.with_path_overlay({tmpdir!r}, {params!r})
+		print("cleanup")
 	""").strip().replace('\n', '; ').format(tmpdir=str(tmpdir), params=params)
 	subprocess.Popen([sys.executable, '-c', script]).wait()
 	out, err = capfd.readouterr()
 	assert str(tmpdir) in out
+	assert "cleanup" in out
