e4b755b9ae06876f8e1bd334108994f4c26d01b9
Jason R. Coombs <jaraco@jaraco.com>
2018-05-15 09:13:11 -0400

    Use declarative config. Requires setuptools 30.3 to install from sdist.

--- setup.py
+++ setup.py
@@ -18,57 +18,7 @@ pkg_resources "managed" namespace packages?
 """
 
 params = dict(
-	name=name,
 	use_scm_version=True,
-	author="Jason R. Coombs",
-	author_email="jaraco@jaraco.com",
-	description=description or name,
-	long_description=long_description,
-	url="https://github.com/jaraco/" + name,
-	packages=setuptools.find_packages(),
-	include_package_data=True,
-	namespace_packages=(
-		name.split('.')[:-1] if nspkg_technique == 'managed'
-		else []
-	),
-	python_requires='>=2.7',
-	install_requires=[
-		'pip',
-	],
-	extras_require={
-		'testing': [
-			# upstream
-			'pytest>=3.5',
-			'pytest-sugar>=0.9.1',
-			'collective.checkdocs',
-			'pytest-flake8',
-
-			# local
-		],
-		'docs': [
-			# upstream
-			'sphinx',
-			'jaraco.packaging>=3.2',
-			'rst.linker>=1.9',
-
-			# local
-		],
-	},
-	setup_requires=[
-		'setuptools_scm>=1.15.0',
-	],
-	classifiers=[
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: Developers",
-		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python :: 2.7",
-		"Programming Language :: Python :: 3",
-	],
-	entry_points={
-		'console_scripts': [
-			'rwt = rwt:run',
-		],
-	},
 )
 if __name__ == '__main__':
 	setuptools.setup(**params)
