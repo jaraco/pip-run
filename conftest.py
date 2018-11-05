535964b0f54e710ebb3f9837e7d0d903b8baa4bc
Jason R. Coombs <jaraco@jaraco.com>
2018-11-05 14:38:18 -0500

    Workaround for Frozenball/pytest-sugar#159.

--- conftest.py
+++ conftest.py
@@ -1,1 +1,12 @@
 collect_ignore = ['examples']
+
+
+def pytest_configure():
+	workaround_sugar_issue_159()
+
+
+def workaround_sugar_issue_159():
+	"https://github.com/Frozenball/pytest-sugar/159"
+	import pytest_sugar
+	pytest_sugar.SugarTerminalReporter.pytest_runtest_logfinish = \
+		lambda self: None
