062ca7abcdb5c2a4b767fe9bd49807e0b9cae3fb
Jason R. Coombs <jaraco@jaraco.com>
2021-01-27 10:09:28 -0500

    Add test and fix to ensure read-deps errors when the specified script does not exist. Fixes #47.

--- pip_run/read-deps.py
+++ pip_run/read-deps.py
@@ -2,19 +2,26 @@
 Emit parameters to pip extracted from a script.
 """
 
-import sys
+import os
+
+import autocommand
 
 from .scripts import DepsReader
 
 
-def run(args=None):
+class ExtantFile(str):
+    def __init__(self, value):
+        if not os.path.isfile(value):
+            raise FileNotFoundError(f"{value} does not exist.")
+
+
+@autocommand.autocommand(__name__)
+def run(script: ExtantFile):
     """
     >>> run(['examples/test-mongodb-covered-query.py'])
     pytest jaraco.mongodb>=3.10
+    >>> run(['does-not-exist'])
+    Traceback (most recent call last):
+    FileNotFoundError: does-not-exist does not exist.
     """
-    (script,) = args or sys.argv[1:]
-    deps = DepsReader.try_read(script)
-    print(' '.join(deps.params()))
-
-
-__name__ == '__main__' and run()
+    print(' '.join(DepsReader.try_read(script).params()))
