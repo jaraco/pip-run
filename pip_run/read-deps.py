1ee4156af9f2f0e9547b0daf0c3d31f50c515cdc
Jason R. Coombs <jaraco@jaraco.com>
2021-01-28 17:36:55 -0500

    Rely on path 15.1 for ExtantFile.

--- pip_run/read-deps.py
+++ pip_run/read-deps.py
@@ -2,26 +2,19 @@
 Emit parameters to pip extracted from a script.
 """
 
-import pathlib
-
 import autocommand
+import path
 
 from .scripts import DepsReader
 
 
-class ExtantFile(str):
-    def __init__(self, value):
-        if not pathlib.Path(value).is_file():
-            raise FileNotFoundError(f"{value} does not exist.")
-
-
 @autocommand.autocommand(__name__)
-def run(script: ExtantFile):
+def run(script: path.ExtantFile):
     """
     >>> run(['examples/test-mongodb-covered-query.py'])
     pytest jaraco.mongodb>=3.10
     >>> run(['does-not-exist'])
     Traceback (most recent call last):
-    FileNotFoundError: does-not-exist does not exist.
+    FileNotFoundError: does-not-exist does not exist as a file.
     """
     print(' '.join(DepsReader.try_read(script).params()))
