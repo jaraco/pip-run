97ccaea9fc3453b08291e34128e461bf45475c8f
Jason R. Coombs <jaraco@jaraco.com>
2022-12-29 20:08:50 -0500

    Remove scripts.run, unused. Improves coverage by 3%.

--- pip_run/scripts.py
+++ pip_run/scripts.py
@@ -1,6 +1,4 @@
-import sys
 import ast
-import tokenize
 import json
 import pathlib
 import abc
@@ -145,20 +143,3 @@ class NotebookDepsReader(DepsReader):
             if cell['cell_type'] == 'code' and not line.startswith('%')
         )
         return cls(''.join(lines))
-
-
-def run(cmdline):
-    """
-    Execute the script as if it had been invoked naturally.
-    """
-    namespace = dict()
-    filename = cmdline[0]
-    namespace['__file__'] = filename
-    namespace['__name__'] = '__main__'
-    sys.argv[:] = cmdline
-
-    open_ = getattr(tokenize, 'open', open)
-    script = open_(filename).read()
-    norm_script = script.replace('\\r\\n', '\\n')
-    code = compile(norm_script, filename, 'exec')
-    exec(code, namespace)
