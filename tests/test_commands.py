562dc69d3724afac368b292f286a998fda745475
Jason R. Coombs <jaraco@jaraco.com>
2023-09-08 19:00:32 -0400

    Split shebang tests into detected, not detected, and invalid encoding.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,31 +1,41 @@
-import codecs
-
 import pytest
 
 from pip_run import commands
 
 
-@pytest.mark.parametrize(
-    "shebang, expect_success",
-    [
-        # simple cases
-        (b"#!/usr/bin/env python", True),
-        (b"#!/usr/bin/env -S pip-run", True),
-        (b"#!/usr/bin/python -W error", True),
-        (b"#/usr/bin/env python", False),
-        (b"!/usr/bin/env python", False),
-        # invalid start byte (not BOM)
-        (b"\xf1#!/usr/bin/env -S python", False),
-        # valid BOM start bytes
-        (codecs.BOM_UTF8 + b"#!/usr/bin/env -S python", True),
-        # invalid start sequence (BOM appears multiple times)
-        (codecs.BOM_UTF8 + codecs.BOM_UTF8 + b"#!/usr/bin/env -S python", False),
-    ],
-)
-def test_shebang_line_detection(tmp_path, shebang, expect_success):
+valid_shebangs = [
+    '#!/usr/bin/env python',
+    '#!/usr/bin/env -S pip-run',
+    '#!/usr/bin/python -W error',
+    # with BOM
+    '#!/usr/bin/env -S python'.encode('utf-8-sig').decode('utf-8'),
+]
+
+invalid_shebangs = [
+    '#/usr/bin/env python',
+    '!/usr/bin/env python',
+]
+
+
+@pytest.mark.parametrize('shebang', valid_shebangs)
+def test_shebang_detected(tmp_path, shebang):
     script = tmp_path / 'script'
-    script.write_bytes(shebang + b'\nprint("Hello world!")')
-    if expect_success:
-        assert commands._has_shebang(script)
-    else:
-        assert not commands._has_shebang(script)
+    script.write_text(f'{shebang}\nprint("Hello world!")', encoding='utf-8')
+    assert commands._has_shebang(script)
+
+
+@pytest.mark.parametrize('shebang', invalid_shebangs)
+def test_shebang_not_detected(tmp_path, shebang):
+    script = tmp_path / 'script'
+    script.write_text(f'{shebang}\nprint("Hello world!")', encoding='utf-8')
+    assert not commands._has_shebang(script)
+
+
+def test_shebang_invalid_encoding(tmp_path):
+    """
+    If the script cannot be decoded in UTF-8, value should be false.
+    """
+    script = tmp_path / 'script'
+    shebang = '\xf1#!/usr/bin/env -S python'
+    script.write_text(f'{shebang}\nprint("Hello world!")', encoding='latin-1')
+    assert not commands._has_shebang(script)
