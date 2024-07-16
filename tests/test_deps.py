a6a3fddcaab428db3833d4bcf2f3ac719e6dd63c
Jason R. Coombs <jaraco@jaraco.com>
2024-07-16 16:20:57 -0400

    Drop support for filtering out satisfied requirements.
    This approach was a performance optimization and intended as a means to augment an existing environment and avoid re-installing packages that were already satisfied. This behavior contradicts the efforts in #104 to run independent of the environment in which pip-run is installed.
    
    Closes #51.

--- tests/test_deps.py
+++ tests/test_deps.py
@@ -1,34 +1,6 @@
-import copy
-
 import pytest
 
 import pip_run.deps as deps
-
-
-class TestInstallCheck:
-    def test_installed(self):
-        assert deps.pkg_installed('pip-run')
-
-    def test_not_installed(self):
-        assert not deps.pkg_installed('not_a_package')
-
-    def test_installed_version(self):
-        assert not deps.pkg_installed('pip-run==0.0')
-
-    def test_not_installed_args(self):
-        args = [
-            '-i',
-            'https://devpi.net',
-            '-r',
-            'requirements.txt',
-            'pip-run',
-            'not_a_package',
-            'pip-run==0.0',
-        ]
-        expected = copy.copy(args)
-        expected.remove('pip-run')
-        filtered = deps.not_installed(args)
-        assert list(filtered) == expected
 
 
 @pytest.mark.usefixtures('retention_strategy')
@@ -55,7 +27,3 @@ def test_target_retention_context():
     """Verify a target exists or can be created."""
     with deps.retention_strategy().context([]) as target:
         target.mkdir(exist_ok=True)
-
-
-def test_url_req_never_installed():
-    assert not deps.pkg_installed('pip_run @git+https://github.com/jaraco/pip-run')
