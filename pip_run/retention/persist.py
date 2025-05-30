e5cbf7cff437fae4942cf1186c7fdddeecf096c5
Jason R. Coombs <jaraco@jaraco.com>
2025-05-29 20:26:25 -0400

    Added support for nice timedelta parsing in max age.

--- pip_run/retention/persist.py
+++ pip_run/retention/persist.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
 import contextlib
+import datetime
+import functools
 import hashlib
 import shutil
 import time
 
 import platformdirs
+import tempora
 
 from .. import deps
 
@@ -53,11 +58,39 @@ def cache_key(args):
 @contextlib.contextmanager
 def context(args, max_age=float('inf')):
     path = paths.user_cache_path.joinpath(cache_key(args))
-    yield clean_if_older(path, max_age)
+    yield clean_if_older(path, parse_duration(max_age))
 
 
 def clean_if_older(path, max_age):
     with contextlib.suppress(FileNotFoundError):
-        age = time.time() - path.stat().st_mtime
+        age = datetime.timedelta(seconds=time.time() - path.stat().st_mtime)
         age > max_age and shutil.rmtree(path)
     return path
+
+
+class Eternity:
+    def __gt__(self, other: datetime.timedelta):
+        return True
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+
+@functools.singledispatch
+def parse_duration(input: str | float):
+    """
+    >>> parse_duration("2 weeks")
+    datetime.timedelta(days=14)
+    >>> parse_duration(30)
+    datetime.timedelta(seconds=30)
+    >>> parse_duration(float('inf'))
+    Eternity
+    """
+    if input == float('inf'):
+        return Eternity()
+    return datetime.timedelta(seconds=input)  # type: ignore[arg-type]
+
+
+@parse_duration.register
+def _(input: str):
+    return tempora.parse_timedelta(input)
